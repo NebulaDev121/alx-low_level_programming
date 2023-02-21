4d458cca970319c45297534e6b3952160b38a40d
Ebri Goodness <121ebrinix@gmail.com>
2023-02-21 15:07:32 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - Computes the absolute value of an integer.
+ *
+ * @n: The integer to compute the absolute value of.
+ *
+ * Return: The absolute value of n.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
