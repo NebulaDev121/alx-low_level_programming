cbf0ba2b7c3cb2a8f2f47c02a33947a1d63a5cf1
Ebri Goodness <121ebrinix@gmail.com>
2023-02-23 19:46:45 -0800

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,29 +11,26 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-	int days_in_year = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? 366 : 365;
-
-	if (month < 1 || month > 12 || day < 1 || day > 31)
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
-	}
+		if (month > 2)
+		{
+			day++;
+		}
 
-	if (month == 2 && day > 29)
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
 	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
 	}
-
-	if (month == 2 && day == 29 && !((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
-	}
-
-	int day_of_year = convert_day(month, day);
-
-	printf("Day of the year: %d\n", day_of_year);
-
-	printf("Remaining days: %d\n", days_in_year - day_of_year);
 }
