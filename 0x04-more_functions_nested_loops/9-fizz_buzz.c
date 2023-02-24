ab7612fc835f4139cdcce436a2c15739ad1373b7
Ebri Goodness <121ebrinix@gmail.com>
2023-02-23 19:18:00 -0800

    Update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the numbers from 1 to 100 followed by a new line.
+ *              For multiples of three, print Fizz instead of the number.
+ *              For multiples of five, print Buzz instead of the number.
+ *              For numbers which are multiples of both three and five,
+ *              print FizzBuzz instead of the number.
+ *
+ * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
