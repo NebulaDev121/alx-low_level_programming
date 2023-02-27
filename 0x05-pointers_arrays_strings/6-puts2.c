e6fe910a15b5c36e653c96d6148a6e5c1739ac34
Ebri Goodness <121ebrinix@gmail.com>
2023-02-27 13:35:09 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: the string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
+
