745bd651153f418a7c93941d4b9bdedf407c6ea3
Ebri Goodness <121ebrinix@gmail.com>
2023-03-03 02:38:02 -0800

    Update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 1, b = 2, c, i;
+
+	printf("%d, %d", a, b);
+
+		for (i = 2; i < 50; i++)
+		{
+			c = a + b;
+
+			printf(", %d", c);
+
+			a = b;
+			b = c;
+		}
+
+	printf("\n");
+
+	return (0);
+}
