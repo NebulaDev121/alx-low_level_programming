85c33a134ba7f4622792af78a63cfc674428525c
Ebri Goodness <121ebrinix@gmail.com>
2023-03-20 15:52:16 -0700

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,10 +11,16 @@
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+        char *name;
+        float age;
+        char *owner;
 };
+
+/* Typedef for struct dog */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
